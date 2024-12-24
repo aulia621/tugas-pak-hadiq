dc941d230c5a6ccd69092b59ee47534356ca36ae
aulia621 <izzaauliam@gmail.com>
2024-12-24 07:02:02 +0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+# Membuat list
+fruits = ['Apel', 'jeruk', 'cherry']
+
+# Mengakses item
+print(fruits[0])  # Output: Apel
+
+# Menambah item
+fruits.append('orange')
+
+# Menghapus item
+fruits.remove('jeruk')
+
+# Mengurutkan list
+fruits.sort()
+
+# Menampilkan list akhir
+print(fruits)  # Output: ['Apel', 'cherry', 'jeruk']
